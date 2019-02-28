498b21074b527c716d0d5b1f8d00583169c18f43
Manoj Majumdar <manojmajumdarb92@gmail.com>
2019-02-28 10:49:54 +0530

    Initial commit of UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { InnerContainerComponent } from './inner-container/inner-container.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    InnerContainerComponent,
+    HomeComponent,
+    ProductsComponent,
+    BoxesComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/inner-container/inner-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inner-container',
+  templateUrl: './inner-container.component.html',
+  styleUrls: ['./inner-container.component.css']
+})
+export class InnerContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
